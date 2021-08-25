32474aacaf2236fae4228ba1af6faf70984be7ab
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2021-08-25 23:00:02 +0530

    add new task button and heading

--- src/components/Eisenboard.js
+++ src/components/Eisenboard.js
@@ -1,13 +1,37 @@
-import { Grid } from "@material-ui/core";
 import React from "react";
+import { Button, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      padding: 20
+    },
+    heading: {
+        fontSize: "24px",
+        color: "#3f51b5",
+        display: "flex",
+        justifyContent: "center"
+    },
+    buttonGrid: {
+        display: "flex",
+        justifyContent: "flex-end"
+    }
+  }));
 
 
 function Eisenboard (props) {
-
+    const classes = useStyles();
     return (
-        <Grid container direction="column">
-            <Grid item>
-                header can contain new task button
+        <Grid container direction="column" className={classes.root}>
+            <Grid item container>
+                <Grid item xs className={classes.heading}>
+                    EISENHOWER MATRIX
+                </Grid>
+                <Grid item xs={2} className={classes.buttonGrid}>
+                    <Button variant="contained" color="primary">
+                        ADD NEW TASK
+                    </Button>
+                </Grid>
             </Grid>
             <Grid item>
                 sections
