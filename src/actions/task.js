411bdf2c0418e53c4da9c858b9e95189256b81a0
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2021-08-27 23:05:59 +0530

    edit and delete action

--- src/actions/task.js
+++ src/actions/task.js
@@ -1,4 +1,4 @@
-import { ADD_NEW_TASK, DND_IN_DIFFERENT_SECTION, DND_IN_SAME_SECTION, GET_TASK_LIST } from "../constants/ActionTypes";
+import { ADD_NEW_TASK, DELETE_TASK, DND_IN_DIFFERENT_SECTION, DND_IN_SAME_SECTION, EDIT_TASK, GET_TASK_LIST } from "../constants/ActionTypes";
 
 export const getTaskList = () => async dispatch => {
     dispatch({ type: GET_TASK_LIST });
@@ -35,4 +35,17 @@ export const addNewTask = (taskID, targetSection, task_name, description) => asy
         description : description
     }
     })
+}
+
+export const editTask = (formValues) => async dispatch => {
+    dispatch({type: EDIT_TASK, providedData: formValues });
+}
+
+export const  deleteTask = (id) => async dispatch => {
+    dispatch(
+        {
+            type : DELETE_TASK,
+            providedData: { id }
+        }
+    )
 }
